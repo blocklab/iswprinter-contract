eb3b452e2ffb919e291ce11689f0aaccb04a5b7e
Axel Hodler <axel@hodler.co>
2017-08-17 10:39:50 +0200

    init contract functionality

--- contracts/Printer.sol
+++ contracts/Printer.sol
@@ -1,4 +1,18 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.11;
 
 contract Printer {
+  mapping (bytes32 => Printable) printables;
+
+  struct Printable {
+    mapping(address => uint) timesUserIsAllowedToPrint;
+  }
+
+  function buyRightToPrintOnce(bytes32 gCodeHash) payable {
+    require (msg.value >= 10000);
+    printables[gCodeHash].timesUserIsAllowedToPrint[msg.sender] = 1;
+  }
+
+  function timesUserIsAllowedToPrint(bytes32 gCodeHash, address user) constant returns (uint) {
+    return printables[gCodeHash].timesUserIsAllowedToPrint[user];
+  }
 }
--- test/printer.js
+++ test/printer.js
@@ -1,9 +1,34 @@
-var Printer = artifacts.require("./Printer.sol");
+var Printer = artifacts.require('./Printer.sol')
 
 contract('Printer', accounts => {
+
+  const ALICE = accounts[0]
+  const G_CODE_HASH = '0x2ed2247fa129d3b73c811ea914c15bf8b21d8cb3a6f9da48042e0ffa1da94430'
+
   it('exists', () => {
     return Printer.deployed().then(instance => {
       assert.ok(instance !== undefined)
     })
   })
-});
+
+  it('allows user who never paid to print zero times', () => {
+    return Printer.deployed().then(instance => {
+      return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, ALICE)
+    }).then(times => {
+      assert.equal(times, 0)
+    })
+  })
+
+  it('allows user paying once to print once', () => {
+    return Printer.deployed().then(instance => {
+      return instance.buyRightToPrintOnce(G_CODE_HASH, {
+        from: ALICE,
+        value: 10000
+      }).then(() => {
+        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, ALICE)
+      }).then(times => {
+        assert.equal(times, 1)
+      })
+    })
+  })
+})
