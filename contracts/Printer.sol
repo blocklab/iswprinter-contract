646463cca7a505e7c68b48e1a80a81799f95e2c9
Axel Hodler <axel@hodler.co>
2017-08-17 11:17:56 +0200

    owner can reset prints back to 0

--- contracts/Printer.sol
+++ contracts/Printer.sol
@@ -2,14 +2,28 @@ pragma solidity ^0.4.11;
 
 contract Printer {
   mapping (bytes32 => Printable) printables;
+  address public owner;
 
   struct Printable {
     mapping(address => uint) timesUserIsAllowedToPrint;
   }
 
+  modifier onlyByOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function Printer() {
+    owner = tx.origin;
+  }
+
   function buyRightToPrintOnce(bytes32 gCodeHash) payable {
     require (msg.value >= 10000);
     printables[gCodeHash].timesUserIsAllowedToPrint[msg.sender] = 1;
+  }
+
+  function resetPrints(bytes32 gCodeHash, address user) onlyByOwner {
+    printables[gCodeHash].timesUserIsAllowedToPrint[user] = 0;
   }
 
   function timesUserIsAllowedToPrint(bytes32 gCodeHash, address user) constant returns (uint) {
--- test/printer.js
+++ test/printer.js
@@ -2,32 +2,45 @@ var Printer = artifacts.require('./Printer.sol')
 
 contract('Printer', accounts => {
 
-  const ALICE = accounts[0]
+  const OSCAR_THE_OWNER = accounts[0]
+  const BOB_THE_USER = accounts[1]
+  const PETE_THE_PRINTER = accounts[2]
   const G_CODE_HASH = '0x2ed2247fa129d3b73c811ea914c15bf8b21d8cb3a6f9da48042e0ffa1da94430'
-
-  it('exists', () => {
-    return Printer.deployed().then(instance => {
-      assert.ok(instance !== undefined)
-    })
-  })
 
   it('allows user who never paid to print zero times', () => {
     return Printer.deployed().then(instance => {
-      return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, ALICE)
-    }).then(times => {
-      assert.equal(times, 0)
+      return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
+    }).then(timesUserCanPrint => {
+      assert.equal(timesUserCanPrint, 0)
     })
   })
 
   it('allows user paying once to print once', () => {
     return Printer.deployed().then(instance => {
       return instance.buyRightToPrintOnce(G_CODE_HASH, {
-        from: ALICE,
+        from: BOB_THE_USER,
         value: 10000
       }).then(() => {
-        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, ALICE)
-      }).then(times => {
-        assert.equal(times, 1)
+        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
+      }).then(timesUserCanPrint => {
+        assert.equal(timesUserCanPrint, 1)
+      })
+    })
+  })
+
+  it('allows the owner to reset the allowed prints back to zero', () => {
+    return Printer.deployed().then(instance => {
+      return instance.buyRightToPrintOnce(G_CODE_HASH, {
+        from: PETE_THE_PRINTER,
+        value: 10000
+      }).then(() => {
+        return instance.resetPrints(G_CODE_HASH, PETE_THE_PRINTER, {
+          from: OSCAR_THE_OWNER
+        })
+      }).then(() => {
+        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, PETE_THE_PRINTER)
+      }).then(timesUserCanPrint => {
+        assert.equal(timesUserCanPrint, 0)
       })
     })
   })
