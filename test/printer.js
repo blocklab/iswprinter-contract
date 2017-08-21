3e230f3b620bcb4bbdacd57b6b27356ef6690c0e
Axel Hodler <axel@hodler.co>
2017-08-21 09:58:44 +0200

    use async await instead of promises

--- test/printer.js
+++ test/printer.js
@@ -14,54 +14,53 @@ contract('Printer', accounts => {
   const G_CODE_HASH = '0x2ed2247fa129d3b73c811ea914c15bf8b21d8cb3a6f9da48042e0ffa1da94430'
   const PRINT_COST = 10000
 
-  it('allows user who never paid to print zero times', () => {
-    return Printer.deployed().then(instance => {
-      return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
-    }).then(timesUserCanPrint => {
-      assert.equal(timesUserCanPrint, 0)
-    })
+  it('allows user who never paid to print zero times', async () => {
+    const printer = await Printer.deployed()
+    const timesUserCanPrint =
+      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
+
+    assert.equal(timesUserCanPrint, 0)
   })
 
-  it('costs ' + PRINT_COST + ' wei to print once', () => {
-    return Printer.deployed().then(instance => {
-      return instance.buyRightToPrintOnce(G_CODE_HASH, {
+  it('costs ' + PRINT_COST + ' wei to print once', async () => {
+    const printer = await Printer.deployed()
+    try {
+      await printer.buyRightToPrintOnce(G_CODE_HASH, {
         from: PETE_THE_PRINTER,
         value: PRINT_COST - 1
-      }).then(function() {
-        assert.fail("buyRightToPrintOnce() was supposed to throw but did not");
-      }).catch(function(error) {
-        assertException(error)
       })
-    })
+      assert.fail("buyRightToPrintOnce() was supposed to throw but did not");
+    } catch (error) {
+      assertException(error)
+    }
   })
 
-  it('allows user paying once to print once', () => {
-    return Printer.deployed().then(instance => {
-      return instance.buyRightToPrintOnce(G_CODE_HASH, {
-        from: BOB_THE_USER,
-        value: PRINT_COST
-      }).then(() => {
-        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
-      }).then(timesUserCanPrint => {
-        assert.equal(timesUserCanPrint, 1)
-      })
+  it('allows user paying once to print once', async () => {
+    const printer = await Printer.deployed()
+
+    await printer.buyRightToPrintOnce(G_CODE_HASH, {
+      from: BOB_THE_USER,
+      value: PRINT_COST
     })
+
+    const timesUserCanPrint =
+      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
+    assert.equal(timesUserCanPrint, 1)
   })
 
-  it('allows the owner to reset the allowed prints back to zero', () => {
-    return Printer.deployed().then(instance => {
-      return instance.buyRightToPrintOnce(G_CODE_HASH, {
-        from: PETE_THE_PRINTER,
-        value: PRINT_COST
-      }).then(() => {
-        return instance.resetPrints(G_CODE_HASH, PETE_THE_PRINTER, {
-          from: OSCAR_THE_OWNER
-        })
-      }).then(() => {
-        return instance.timesUserIsAllowedToPrint.call(G_CODE_HASH, PETE_THE_PRINTER)
-      }).then(timesUserCanPrint => {
-        assert.equal(timesUserCanPrint, 0)
-      })
+  it('allows the owner to reset the allowed prints back to zero', async () => {
+    const printer = await Printer.deployed()
+    await printer.buyRightToPrintOnce(G_CODE_HASH, {
+      from: PETE_THE_PRINTER,
+      value: PRINT_COST
     })
+
+    await printer.resetPrints(G_CODE_HASH, PETE_THE_PRINTER, {
+      from: OSCAR_THE_OWNER
+    })
+
+    const timesUserCanPrint =
+      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, PETE_THE_PRINTER)
+    assert.equal(timesUserCanPrint, 0)
   })
 })
