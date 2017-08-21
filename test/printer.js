44aa8fe50bd7a3803bb5ebd6d7e7bcd0a3a3f63e
Axel Hodler <axel@hodler.co>
2017-08-21 10:04:23 +0200

    improve structure

--- test/printer.js
+++ test/printer.js
@@ -1,9 +1,15 @@
 const Printer = artifacts.require('./Printer.sol')
 
+const G_CODE_HASH = '0x2ed2247fa129d3b73c811ea914c15bf8b21d8cb3a6f9da48042e0ffa1da94430'
+
 let assertException = error => {
   if (error.toString().indexOf("invalid opcode") == -1) {
     assert(false, error.toString());
   }
+}
+
+const timesUserCanPrint = async (printer, user) => {
+  return printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, user)
 }
 
 contract('Printer', accounts => {
@@ -11,15 +17,14 @@ contract('Printer', accounts => {
   const OSCAR_THE_OWNER = accounts[0]
   const BOB_THE_USER = accounts[1]
   const PETE_THE_PRINTER = accounts[2]
-  const G_CODE_HASH = '0x2ed2247fa129d3b73c811ea914c15bf8b21d8cb3a6f9da48042e0ffa1da94430'
   const PRINT_COST = 10000
 
   it('allows user who never paid to print zero times', async () => {
     const printer = await Printer.deployed()
-    const timesUserCanPrint =
-      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
 
-    assert.equal(timesUserCanPrint, 0)
+    const allowedPrintsNumber = await timesUserCanPrint(printer, BOB_THE_USER)
+
+    assert.equal(allowedPrintsNumber, 0)
   })
 
   it('costs ' + PRINT_COST + ' wei to print once', async () => {
@@ -43,9 +48,8 @@ contract('Printer', accounts => {
       value: PRINT_COST
     })
 
-    const timesUserCanPrint =
-      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, BOB_THE_USER)
-    assert.equal(timesUserCanPrint, 1)
+    const allowedPrintsNumber = await timesUserCanPrint(printer, BOB_THE_USER)
+    assert.equal(allowedPrintsNumber, 1)
   })
 
   it('allows the owner to reset the allowed prints back to zero', async () => {
@@ -59,8 +63,7 @@ contract('Printer', accounts => {
       from: OSCAR_THE_OWNER
     })
 
-    const timesUserCanPrint =
-      await printer.timesUserIsAllowedToPrint.call(G_CODE_HASH, PETE_THE_PRINTER)
-    assert.equal(timesUserCanPrint, 0)
+    const allowedPrintsNumber = await timesUserCanPrint(printer, PETE_THE_PRINTER)
+    assert.equal(allowedPrintsNumber, 0)
   })
 })
